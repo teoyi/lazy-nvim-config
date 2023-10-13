f51394e3174e0115589a1d5761ddc0139be50282
luke-teo <yipengteo.luke@gmail.com>
2023-10-13 20:39:39 +0900

    feat: configured lazy nvim

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      -- add a keymap to browse plugin files
+      -- stylua: ignore
+      {
+        "<leader>fp",
+        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+        desc = "Find Plugin File",
+      },
+      {
+        "<leader>fg",
+        function()
+          require("telescope.builtin").live_grep({ cwd = require("lazy.core.config").options.root })
+        end,
+        desc = "Find File Using Grep",
+      },
+    },
+    -- change some options
+    opts = {
+      defaults = {
+        layout_strategy = "horizontal",
+        layout_config = { prompt_position = "bottom" },
+        sorting_strategy = "descending",
+        winblend = 0,
+      },
+    },
+  },
+
+  -- add telescope-fzf-native
+  {
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+  },
+}
