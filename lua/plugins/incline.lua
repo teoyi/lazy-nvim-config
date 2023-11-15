13a0d1a299898d20a780fb96048f05489472c61d
Luke Teo <luke.teo@colorkrew.com>
2023-11-15 09:30:36 +0900

    fix: changed incline to only show file name

--- lua/plugins/incline.lua
+++ lua/plugins/incline.lua
@@ -7,7 +7,7 @@ return {
       window = { margin = { vertical = 0, horizontal = 1 } },
       hide = { cursorline = true },
       render = function(props)
-        local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ";t")
+        local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
         if vim.bo[props.buf].modified then
           filename = "[+]" .. filename
         end
