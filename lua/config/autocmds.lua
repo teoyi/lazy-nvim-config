883f876fbfbc9157d33d68d555de5be0d2032e3a
Luke Teo <luke.teo@colorkrew.com>
2023-12-06 11:00:25 +0900

    fix: removed animate, remove autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,33 +1,33 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local bufnr = args.buf
-
-    -- auto show diagnostic when cursor hold
-    vim.api.nvim_create_autocmd("CursorHold", {
-      buffer = bufnr,
-      callback = function()
-        local float_opts = {
-          focusable = false,
-          close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-        }
-
-        if not vim.b.diagnostics_pos then
-          vim.b.diagnostics_pos = { nil, nil }
-        end
-
-        local cursor_pos = vim.api.nvim_win_get_cursor(0)
-        if
-          (cursor_pos[1] ~= vim.b.diagnostics_pos[1] or cursor_pos[2] ~= vim.b.diagnostics_pos[2])
-          and #vim.diagnostic.get() > 0
-        then
-          vim.diagnostic.open_float(nil, float_opts)
-        end
-
-        vim.b.diagnostics_pos = cursor_pos
-      end,
-    })
-  end,
-})
+-- vim.api.nvim_create_autocmd("LspAttach", {
+--   callback = function(args)
+--     local bufnr = args.buf
+--
+--     -- auto show diagnostic when cursor hold
+--     vim.api.nvim_create_autocmd("CursorHold", {
+--       buffer = bufnr,
+--       callback = function()
+--         local float_opts = {
+--           focusable = false,
+--           close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+--         }
+--
+--         if not vim.b.diagnostics_pos then
+--           vim.b.diagnostics_pos = { nil, nil }
+--         end
+--
+--         local cursor_pos = vim.api.nvim_win_get_cursor(0)
+--         if
+--           (cursor_pos[1] ~= vim.b.diagnostics_pos[1] or cursor_pos[2] ~= vim.b.diagnostics_pos[2])
+--           and #vim.diagnostic.get() > 0
+--         then
+--           vim.diagnostic.open_float(nil, float_opts)
+--         end
+--
+--         vim.b.diagnostics_pos = cursor_pos
+--       end,
+--     })
+--   end,
+-- })
