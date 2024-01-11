a53c676f605b083fcca07085cb076bda99724f77
Luke Teo <luke.teo@colorkrew.com>
2024-01-11 09:47:49 +0900

    feat: added autocmd to reveal quotes for json and md

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -31,3 +31,12 @@
 --     })
 --   end,
 -- })
+--
+-- Disable the concealing in some file formats
+-- The default conceallevel is 3 in LazyVim
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "json", "jsonc", "markdown" },
+  callback = function()
+    vim.wo.conceallevel = 0
+  end,
+})
