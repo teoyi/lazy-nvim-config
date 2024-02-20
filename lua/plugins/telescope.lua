05f4740fe291b373babd9aa954b1074bd7e41a84
Luke Teo <luke.teo@colorkrew.com>
2024-02-20 11:15:54 +0900

    enabled telescope file broswer

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -16,28 +16,28 @@ return {
         end,
         desc = "Find File Using Grep",
       },
-      -- {
-      --   ";e",
-      --   function()
-      --     local telescope = require("telescope")
-      --
-      --     local function telescope_buffer_dir()
-      --       return vim.fn.expand("%:p:h")
-      --     end
-      --
-      --     telescope.extensions.file_browser.file_browser({
-      --       path = "%:p:h",
-      --       cwd = telescope_buffer_dir(),
-      --       respect_gitignore = false,
-      --       hidden = true,
-      --       grouped = true,
-      --       previewer = true,
-      --       initial_mode = "normal",
-      --       layout_config = { height = 40 },
-      --     })
-      --   end,
-      --   desc = "Open File Browser with the path of the current buffer",
-      -- },
+      {
+        ";e",
+        function()
+          local telescope = require("telescope")
+
+          local function telescope_buffer_dir()
+            return vim.fn.expand("%:p:h")
+          end
+
+          telescope.extensions.file_browser.file_browser({
+            path = "%:p:h",
+            cwd = telescope_buffer_dir(),
+            respect_gitignore = false,
+            hidden = true,
+            grouped = true,
+            previewer = true,
+            initial_mode = "normal",
+            layout_config = { height = 40 },
+          })
+        end,
+        desc = "Open File Browser with the path of the current buffer",
+      },
     },
     -- change some options
     opts = {
@@ -50,8 +50,8 @@ return {
     },
     config = function(_, opts)
       local telescope = require("telescope")
-      -- local actions = require("telescope.actions")
-      -- local fb_actions = require("telescope").extensions.file_browser.actions
+      local actions = require("telescope.actions")
+      local fb_actions = require("telescope").extensions.file_browser.actions
 
       opts.defaults = vim.tbl_deep_extend("force", opts.defaults, {
         wrap_results = true,
@@ -73,38 +73,38 @@ return {
         },
       }
       opts.extensions = {
-        -- file_browser = {
-        --   theme = "dropdown",
-        --   -- disables netrw and use telescope-file-browser in its place
-        --   hijack_netrw = true,
-        --   mappings = {
-        --     -- your custom insert mode mappings
-        --     ["n"] = {
-        --       -- your custom normal mode mappings
-        --       ["N"] = fb_actions.create,
-        --       ["h"] = fb_actions.goto_parent_dir,
-        --       ["/"] = function()
-        --         vim.cmd("startinsert")
-        --       end,
-        --       ["<C-u>"] = function(prompt_bufnr)
-        --         for i = 1, 10 do
-        --           actions.move_selection_previous(prompt_bufnr)
-        --         end
-        --       end,
-        --       ["<C-d>"] = function(prompt_bufnr)
-        --         for i = 1, 10 do
-        --           actions.move_selection_next(prompt_bufnr)
-        --         end
-        --       end,
-        --       ["<PageUp>"] = actions.preview_scrolling_up,
-        --       ["<PageDown>"] = actions.preview_scrolling_down,
-        --     },
-        --   },
-        -- },
+        file_browser = {
+          theme = "dropdown",
+          -- disables netrw and use telescope-file-browser in its place
+          hijack_netrw = true,
+          mappings = {
+            -- your custom insert mode mappings
+            ["n"] = {
+              -- your custom normal mode mappings
+              ["N"] = fb_actions.create,
+              ["h"] = fb_actions.goto_parent_dir,
+              ["/"] = function()
+                vim.cmd("startinsert")
+              end,
+              ["<C-u>"] = function(prompt_bufnr)
+                for i = 1, 10 do
+                  actions.move_selection_previous(prompt_bufnr)
+                end
+              end,
+              ["<C-d>"] = function(prompt_bufnr)
+                for i = 1, 10 do
+                  actions.move_selection_next(prompt_bufnr)
+                end
+              end,
+              ["<PageUp>"] = actions.preview_scrolling_up,
+              ["<PageDown>"] = actions.preview_scrolling_down,
+            },
+          },
+        },
       }
       telescope.setup(opts)
       require("telescope").load_extension("fzf")
-      -- require("telescope").load_extension("file_browser")
+      require("telescope").load_extension("file_browser")
     end,
   },
 
@@ -113,7 +113,7 @@ return {
     "telescope.nvim",
     dependencies = {
       "nvim-telescope/telescope-fzf-native.nvim",
-      -- "nvim-telescope/telescope-file-browser.nvim",
+      "nvim-telescope/telescope-file-browser.nvim",
       build = "make",
       config = function()
         require("telescope").load_extension("fzf")
